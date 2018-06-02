df7155c0813b7d6ebf7a549ef7b347c01087d65b
Giles Lewey <gileslewey@gmail.com>
2018-06-02 15:39:33 -0700

    routes added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'Early altered coins (1750s - 1913)')
+Category.create(name: 'Classic old hobo nickels (1913 - 1940)')
+Category.create(name: 'Later old hobo nickels (1940 - 1980)')
+Category.create(name: 'EModern hobo nickels (since 1980)')
